d52676e00050cd18d93aae041922780de3442102
BlockOfGaming <alesiobaca@gmail.com>
2022-06-04 17:53:37 +0300

    Changed the probabilites to be correct, changed tick rate, added generator icon and interpolation with it

--- Pop The Cell.gd
+++ Pop The Cell.gd
@@ -8,25 +8,60 @@ onready var scoreLabel = get_node("CenterContainer/VBoxContainer/Scores/Score")
 onready var highScoreLabel = get_node("CenterContainer/VBoxContainer/Scores/Highscore")
 
 # Progress buttons
-onready var buttons1 = get_node("CenterContainer/VBoxContainer/Vertical/ProgressBar/Margin/Vertical")
-onready var buttons2 = get_node("CenterContainer/VBoxContainer/Vertical/ProgressBar2/Margin/Vertical")
-onready var buttons3 = get_node("CenterContainer/VBoxContainer/Vertical/ProgressBar3/Margin/Vertical")
+onready var buttons1 = get_node("CenterContainer/VBoxContainer/Vertical/Vertical/ProgressBar/Margin/Vertical")
+onready var buttons2 = get_node("CenterContainer/VBoxContainer/Vertical/Vertical2/ProgressBar/Margin/Vertical")
+onready var buttons3 = get_node("CenterContainer/VBoxContainer/Vertical/Vertical3/ProgressBar/Margin/Vertical")
+
+onready var buttons = [buttons1, buttons2, buttons3]
+
+# Textures
+onready var generator1 = get_node("CenterContainer/VBoxContainer/Vertical/Vertical/Texture")
+onready var generator2 = get_node("CenterContainer/VBoxContainer/Vertical/Vertical2/Texture")
+onready var generator3 = get_node("CenterContainer/VBoxContainer/Vertical/Vertical3/Texture")
+
+onready var generators = [generator1, generator2, generator3]
 
 # Other nodes
 onready var reStartButton = get_node("CenterContainer/VBoxContainer/(Re)Start")
+onready var tween = get_node("Tween")
 onready var verticalContainer = get_node("CenterContainer/VBoxContainer")
 # Get the original size of verticalContainer
 onready var origVerticalContainerSize = verticalContainer.rect_size
 
-# Create the scores
+# Create the score variables
 var score = 0
 var highscore = score
 
-# Create the tick var
+var dirs = [0, 0, 0]
+
 var tick = 0
+var tick_rate = 4
+var tick_time = 1.0 / tick_rate
+
+
+func load_hi():
+	var file = File.new()
+	
+	if file.file_exists("user://highscore"):
+		file.open("user://highscore", file.READ)
+		highscore = file.get_64()
+		highScoreLabel.text = str(highscore)
+	
+	file.close()
+
+func save_hi():
+	var file = File.new()
+	
+	file.open("user://highscore", file.WRITE)
+	file.store_64(highscore)
+	
+	file.close()
 
 
 func _ready():
+	randomize()
+	load_hi()
+	
 	# Connect all the buttons
 	for i in range(7):
 		buttons1.get_child(i).connect("pressed", self, "_on_1_pressed", [i])
@@ -34,7 +69,7 @@ func _ready():
 		buttons3.get_child(i).connect("pressed", self, "_on_3_pressed", [i])
 	
 	# Set width of window to not be the smallest possible
-	OS.window_size[0] = 1024
+	OS.window_size = Vector2(1024, 600)
 
 
 func create_timer():
@@ -42,7 +77,7 @@ func create_timer():
 	var timer = Timer.new()
 	
 	# Set timer settings and connect it
-	timer.wait_time = 0.2
+	timer.wait_time = tick_time
 	timer.one_shot = true
 	timer.connect("timeout", self, "_on_Timer_timeout", [timer])
 	
@@ -70,6 +105,8 @@ func _on_ReStart_pressed():
 		buttons2.get_child(i).disabled = true
 		buttons3.get_child(i).disabled = true
 	
+	dirs = [0, 0, 0]
+	
 	# Start the ticks
 	create_timer()
 	
@@ -79,14 +116,27 @@ func _on_ReStart_pressed():
 	reStartButton.text = "Restart"
 
 
-func do_random_add(buttons):
-	# There's a 1/4 chance that one button is going to be added
-	if (randi() % 4) == 0:
+func do_random_add(index):
+	# There's a 1/2 chance that the "generator" is going to turn left or right
+	if randi() % 2 == 0:
+		dirs[index] += 1
+	else:
+		dirs[index] -= 1
+	tween.interpolate_property(
+		generators[index],
+		"rect_rotation",
+		generators[index].rect_rotation,
+		dirs[index] * 90,
+		tick_time
+	)
+	tween.start()
+	
+	if abs(dirs[index] % 4) == 0:
 		for i in range(6, -1, -1):
-			if buttons.get_child(i).disabled:
-				buttons.get_child(i).disabled = false
+			if buttons[index].get_child(i).disabled:
+				buttons[index].get_child(i).disabled = false
 				break
-		if !buttons.get_child(0).disabled: return true
+		if !buttons[index].get_child(0).disabled: return true
 	
 	return false
 
@@ -99,12 +149,14 @@ func _on_Timer_timeout(timer):
 	# Score updates every 3 ticks
 	if tick % 3 == 0:
 		score += 1
+		if score > highscore: highscore = score
 	
 	# Do the random addition to the button lengths, if any button has
 	# a score bigger or equal to 7, stop the game and show the restart button
-	if do_random_add(buttons1) or do_random_add(buttons2) or do_random_add(buttons3):
+	if do_random_add(0) or do_random_add(1) or do_random_add(2):
 		reStartButton.disabled = false
 		reStartButton.modulate.a = 1
+		save_hi()
 		return
 	
 	# Create a new timer for the next tick
@@ -112,21 +164,18 @@ func _on_Timer_timeout(timer):
 
 
 func _process(_delta):
-	# Set score label
+	# Set score and highscore labels
 	scoreLabel.text = "Score: " + str(score)
-	
-	# Set highscore and label
-	if score > highscore: highscore = score
 	highScoreLabel.text = "Highscore: " + str(highscore)
 
 
-func do_button(buttons, i):
+func do_button(index, i):
 	if reStartButton.disabled:
-		buttons.get_child(i).disabled = true
+		buttons[index].get_child(i).disabled = true
 		create_sound()
 
 
 # Subtract 1 from the buttons if the game is started
-func _on_1_pressed(i): do_button(buttons1, i)
-func _on_2_pressed(i): do_button(buttons2, i)
-func _on_3_pressed(i): do_button(buttons3, i)
+func _on_1_pressed(i): do_button(0, i)
+func _on_2_pressed(i): do_button(1, i)
+func _on_3_pressed(i): do_button(2, i)
